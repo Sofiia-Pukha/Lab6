4d77c5e89d30ebfd94610b1dcb37710bf4045e2f
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-21 23:56:10 +0200

    add basic setup and includes

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <coroutine>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+    srand(time(0));
+    
+    cout << "Laboratory work started..." << endl;
+    
+    return 0;
+}
